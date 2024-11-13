366446ba06ebf71f2ad9b379ad9f37261cc37e27
Gabriel G <gabriel59kg@gmail.com>
2024-11-13 12:03:49 -0500

    add forms

--- fast_stack/app.py
+++ fast_stack/app.py
@@ -1,76 +1,101 @@
-from datetime import date
+from __future__ import annotations as _annotations
 
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
-from fastui import FastUI, AnyComponent, prebuilt_html, components as c
-from fastui.components.display import DisplayMode, DisplayLookup
-from fastui.events import GoToEvent, BackEvent
-from pydantic import BaseModel, Field
+import sys
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, PlainTextResponse
+from fastui import prebuilt_html
+from fastui.auth import fastapi_auth_exception_handling
+from fastui.dev import dev_fastapi_app
+from httpx import AsyncClient
 
+from fastapi import APIRouter
+from fastui import AnyComponent, FastUI
+from fastui import components as c
 
-class User(BaseModel):
-    id: int
-    name: str
-    dob: date = Field(title='Date of Birth')
+from .shared import demo_page
+from .forms import router as forms_router
 
-
-# define some users
-users = [
-    User(id=1, name='John', dob=date(1990, 1, 1)),
-    User(id=2, name='Jack', dob=date(1991, 1, 1)),
-    User(id=3, name='Jill', dob=date(1992, 1, 1)),
-    User(id=4, name='Jane', dob=date(1993, 1, 1)),
-]
+main_router = APIRouter()
 
 
-@app.get("/api/", response_model=FastUI, response_model_exclude_none=True)
-def users_table() -> list[AnyComponent]:
-    """
-    Show a table of four users, `/api` is the endpoint the frontend will connect to
-    when a user visits `/` to fetch components to render.
-    """
-    return [
-        c.Page(  # Page provides a basic container for components
-            components=[
-                c.Heading(text='Users', level=2),  # renders `<h2>Users</h2>`
-                c.Table(
-                    data=users,
-                    # define two columns for the table
-                    columns=[
-                        # the first is the users, name rendered as a link to their profile
-                        DisplayLookup(field='name', on_click=GoToEvent(url='/user/{id}/')),
-                        # the second is the date of birth, rendered as a date
-                        DisplayLookup(field='dob', mode=DisplayMode.date),
-                    ],
-                ),
-            ]
-        ),
-    ]
+@main_router.get('/', response_model=FastUI, response_model_exclude_none=True)
+def api_index() -> list[AnyComponent]:
+    # language=markdown
+    markdown = """\
+This site provides a demo of [FastUI](https://github.com/pydantic/FastUI), the code for the demo
+is [here](https://github.com/pydantic/FastUI/tree/main/demo).
+
+You can find the documentation for FastUI [here](https://docs.pydantic.dev/fastui/).
+
+The following components are demonstrated:
+
+* `Markdown` — that's me :-)
+* `Text`— example [here](/components#text)
+* `Paragraph` — example [here](/components#paragraph)
+* `PageTitle` — you'll see the title in the browser tab change when you navigate through the site
+* `Heading` — example [here](/components#heading)
+* `Code` — example [here](/components#code)
+* `Button` — example [here](/components#button-and-modal)
+* `Link` — example [here](/components#link-list)
+* `LinkList` — example [here](/components#link-list)
+* `Navbar` — see the top of this page
+* `Footer` — see the bottom of this page
+* `Modal` — static example [here](/components#button-and-modal), dynamic content example [here](/components#dynamic-modal)
+* `ServerLoad` — see [dynamic modal example](/components#dynamic-modal) and [SSE example](/components#server-load-sse)
+* `Image` - example [here](/components#image)
+* `Iframe` - example [here](/components#iframe)
+* `Video` - example [here](/components#video)
+* `Toast` - example [here](/components#toast)
+* `Table` — See [cities table](/table/cities) and [users table](/table/users)
+* `Pagination` — See the bottom of the [cities table](/table/cities)
+* `ModelForm` — See [forms](/forms/login)
+
+Authentication is supported via:
+* token based authentication — see [here](/auth/login/password) for an example of password authentication
+* GitHub OAuth — see [here](/auth/login/github) for an example of GitHub OAuth login
+"""
+    return demo_page(c.Markdown(text=markdown))
+
+@main_router.get('/{path:path}', status_code=404)
+async def api_404():
+    # so we don't fall through to the index page
+    return {'message': 'Not Found'}
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    async with AsyncClient() as client:
+        app_.state.httpx_client = client
+        yield
+
+frontend_reload = '--reload' in sys.argv
+if frontend_reload:
+    # dev_fastapi_app reloads in the browser when the Python source changes
+    app = dev_fastapi_app(lifespan=lifespan)
+else:
+    app = FastAPI(lifespan=lifespan)
+
+fastapi_auth_exception_handling(app)
+
+# app.include_router(components_router, prefix='/api/components')
+# app.include_router(sse_router, prefix='/api/components')
+# app.include_router(table_router, prefix='/api/table')
+app.include_router(forms_router, prefix='/api/forms')
+# app.include_router(auth_router, prefix='/api/auth')
+app.include_router(main_router, prefix='/api')
 
 
-@app.get("/api/user/{user_id}/", response_model=FastUI, response_model_exclude_none=True)
-def user_profile(user_id: int) -> list[AnyComponent]:
-    """
-    User profile page, the frontend will fetch this when the user visits `/user/{id}/`.
-    """
-    try:
-        user = next(u for u in users if u.id == user_id)
-    except StopIteration:
-        raise HTTPException(status_code=404, detail="User not found")
-    return [
-        c.Page(
-            components=[
-                c.Heading(text=user.name, level=2),
-                c.Link(components=[c.Text(text='Back')], on_click=BackEvent()),
-                c.Details(data=user),
-            ]
-        ),
-    ]
+@app.get('/robots.txt', response_class=PlainTextResponse)
+async def robots_txt() -> str:
+    return 'User-agent: *\nAllow: /'
+
+
+@app.get('/favicon.ico', status_code=404, response_class=PlainTextResponse)
+async def favicon_ico() -> str:
+    return 'page not found'
 
 
 @app.get('/{path:path}')
 async def html_landing() -> HTMLResponse:
-    """Simple HTML page which serves the React app, comes last as it matches all paths."""
     return HTMLResponse(prebuilt_html(title='FastUI Demo'))
