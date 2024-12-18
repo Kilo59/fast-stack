773a09bead2e44ce49bac6da97cade36d0a632c2
Gabriel G <gabriel59kg@gmail.com>
2024-12-18 13:35:47 -0500

    basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,47 @@
+import typing as t
+
 import pytest
+from starlette.testclient import TestClient
 
 
-def test_foo():
-    assert "foobar"
+@pytest.fixture
+def client() -> t.Generator[TestClient, None, None]:
+    from fast_stack.app import app
+
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/",
+        "/auth/login/password",
+        "/auth/login/github",
+        "/components",
+        "/components#button-and-modal",
+        "/components#link-list",
+        "/components#link-list",
+        "/components#dynamic-modal",
+        "/components#server-load-sse",
+        "/components#image",
+        "/components#iframe",
+        "/components#video",
+        "/components#toast",
+        "/table/cities",
+        "/table/users",
+        "/table/cities",
+        "/forms/login",
+        "/auth/login/password",
+        "/auth/login/github",
+        "/robots.txt",
+    ],
+)
+def test_gets(path: str, client: TestClient) -> None:
+    response = client.get(path)
+    print(response)
+    print(response.content)
+    assert response.status_code == 200
 
 
 if __name__ == "__main__":
